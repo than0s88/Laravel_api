667405367942f718012155fc78b5a0bf13279b49
Paulo <prime.thanos88@gmail.com>
2022-09-29 20:56:19 +0800

    first commit

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    protected $model = Customer::class;
+
+    public function definition()
+    {
+        $type = $this->faker->randomElement(['I','B']);
+        $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
+
+        return [ 
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'postal_code' => $this->faker->postCode()
+        ];
+    }
+}
