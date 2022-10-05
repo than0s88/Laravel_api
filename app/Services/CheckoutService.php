632dda59be4e61256cf4c6f6179818a723974ff8
Paulo <prime.thanos88@gmail.com>
2022-10-05 23:33:55 +0800

    Added Test Unit

---
+++ app/Services/CheckoutService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+class CheckoutService
+{
+    private $pricing_rules;
+    public $total = 0;
+    public function __construct($pricing_rules)
+    {
+    $this->pricing_rules = $pricing_rules;
+    }
+
+    public function scan(string $item){
+        $product = Product::where('product_code', $item)->first();
+        if($product){
+            //add to cart
+            $this->total = 0;
+        }
+    }
+}
